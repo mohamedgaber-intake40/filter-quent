a85e7e71c4fb0cc761eddf8e130c3206437c7404
Mohamed Gaber <gaberm514@gmail.com>
2021-06-13 19:45:34 +0200

    version 1.0

---
+++ src/Filter.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace Filter;
+
+
+use Filter\Utilities\FilterCollection;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
+
+abstract class Filter
+{
+    /**
+     * @var Builder
+     */
+    protected $query;
+
+    protected function __construct($query, FilterCollection $data)
+    {
+        $this->query = $query;
+        $this->filter($data);
+    }
+
+
+    /**
+     * @param array $data
+     * @param Builder $query
+     * @return Builder $query
+     */
+    public static function create(Builder $query, array $data)
+    {
+        $data = \filterCollection($data);
+        return ( new static($query, $data) )->query;
+    }
+
+    /**
+     * @param FilterCollection $collection
+     */
+    protected function filter(FilterCollection $collection)
+    {
+        $reflector = new \ReflectionClass(static::class);
+        foreach ( $reflector->getMethods() as $method ) {
+            $matches = [];
+            if(preg_match('/^filter([\S]+)$/i', $method->getName(), $matches)){
+                $property = $collection->{Str::snake($matches[ 1 ])};
+                if($property){
+                    $this->{$method->getName()}($property);
+                }
+            }
+        }
+    }
+
+}
